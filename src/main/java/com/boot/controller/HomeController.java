3693592787aa91709fd98826e41d9c62b3236532
Tiago Melo <tiagotele@gmail.com>
2017-03-05 15:28:44 -0300

    First commit

---
+++ src/main/java/com/boot/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Tiago on 03/03/17.
+ */
+@RestController
+public class HomeController {
+    @RequestMapping("")
+    public String home(){
+        return "Das boot, reporting for duty!";
+    }
+}
